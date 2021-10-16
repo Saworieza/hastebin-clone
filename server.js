24bc551234e19c3c37735d0865d134a57ea560f0
saworieza <saworieza@gmail.com>
2021-10-16 16:08:39 +0300

    final db changes

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express")
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+const Document = require("./models/Document")
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/hastebin", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
 
 app.get("/", (req, res) => {
     const code = `Welcome to WasteBin!
@@ -9,11 +17,32 @@ app.get("/", (req, res) => {
     Use the commands in the top right corner
     to create a new file to share with others.`
 
-    res.render("code-display", { code })
+    res.render("code-display", { code, language: 'plaintext' })
 })
 
 app.get('/new', (req, res) => {
     res.render("new")
+})
+
+app.post('/save', async(req, res) => {
+    const value = req.body.value
+    try {
+        const document = await Document.create({ value })
+        res.redirect(`/${document.id}`)
+    } catch (e) {
+        res.render("new", { value })
+    }
+    console.log(value)
+})
+
+app.get('/:id', async(req, res) => {
+    const id = req.params.id
+    try {
+        const await Document.findById(id)
+        res.render('code-display', { code: document.value })
+    } catch (e) {
+        res.redirect('/')
+    }
 })
 
 app.listen(3000)
